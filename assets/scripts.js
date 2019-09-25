61f3e6f2225b0a60f5948e4ebec948bbb1f8e4be
EC2 Default User <ec2-user@ip-172-31-80-112.ec2.internal>
2019-09-25 20:02:08 +0000

    files for hw2

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,47 +12,71 @@ const navIcon = "#navIcon",
     status = "#status-message";
 
 // Functions
-function checkStock(){
-    if( $(stock).val() == 0 ){
-        return false;
-    }
-    else
-        return true;
-};
-
-function updateStatus(message){
-    if (message == "oos") {
-        $(status).show();
-        $(status).html("Error: This product is out of stock.");
-        $(status).addClass("error");
+function toggleMenu(menu){
+    switch (menu) {
+        case 'cart':
+            $(navMenu).slideUp();
+            if( $(navIcon).hasClass("open") )
+                $(navIcon).removeClass("open");
+            $(cart).slideToggle();
+            break;
+        case 'nav':
+            $(cart).slideUp();
+            $(navMenu).slideToggle();
+            break;
+        default:
+            break;
     }
 }
+
+function updateStatus(message){
+    if (message) {
+        $(status).slideDown();
+        $(status).html("This product is out of stock.");
+        $(status).addClass("error");
+        if( $(status).hasClass("success") )
+            $(status).removeClass("success");
+    }
+    else{
+        $(status).slideDown();
+        $(status).html("Item added to cart.")
+        $(status).addClass("success");
+        if( $(status).hasClass("error") )
+            $(status).removeClass("error");
+    }
+};
 
 function setOutOfStock(){
     $(stock).val("0");
     $(add2CartBtn).hide();
     $(outOfStockBtn).show();
-    updateStatus("oos");
+    updateStatus(true);
 };
 
 function resetPage(){
     $(stock).val("5");
     $(add2CartBtn).show();
     $(outOfStockBtn).hide();
-    $(status).hide();
+    $(status).slideUp();
+    $(status).classList = "";
 };
+
+
 
 // Event Listeners
 $(navIcon).on('click', function(){
     $(this).toggleClass('open');
-    $(navMenu).slideToggle();
+    toggleMenu("nav");
 });
 
 $(cartIcon).on('click', function(){
-    if( $(navIcon).hasClass('open'))
-        $(navMenu).trigger('click');
-        
-    $(cart).slideToggle();
+    toggleMenu("cart");
+});
+
+$(add2CartBtn).on('click', function() {
+   if ( $(stock).val() != "0" ) {
+       updateStatus(false);
+   }
 });
 
 $(reset).on('click', function() {
